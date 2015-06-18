7da54bfe166f940618a127e4ac5f44f6b7a3d0bb
Dario Garcia <dariog@lsi.upc.edu>
2015-06-18 10:58:40 +0200

    Fixed error in euclidean distance calc.

--- include/Util.hpp
+++ include/Util.hpp
@@ -2,11 +2,12 @@
 #define UTIL_H
 
 #include "Image.hpp"
+#include "CNNLayer.hpp"
 
 class Util {
   public:
     static void generate_random_string(char *s, const int len);
-    static float euclideanDistanceActivations(const Image &img1, const Image &img2);
+    static float euclideanDistanceActivations(const map<string,CNNLayer> &layers, const Image &img1, const Image &img2);
 }; // UTIL
 
 #endif
--- src/Util.cpp
+++ src/Util.cpp
@@ -15,24 +15,24 @@ void Util::generate_random_string(char *s, const int len) {
   s[len] = 0;
 }
 
-float Util::euclideanDistanceActivations(const Image &img1, const Image &img2){
+float Util::euclideanDistanceActivations(const map<string,CNNLayer> &layers, const Image &img1, const Image &img2){
   float distance=0; 
   //For each layer
-  for(map<string,map<int,float> >::const_iterator it = img1.getActivations().begin();
-    it!=img1.getActivations().end(); it++){
-    //For each feature
-    for(map<int,float>::const_iterator it2 = it->second.begin(); it2!=it->second.end(); it2++){
-      //If layer exists in img2
-      if(img2.getActivations().find(it->first)!=img2.getActivations().end()){
-        //If feature exists in img2
-        if(img2.getActivations()[it->first].find(it2->first)!=img2.getActivations()[it->first].end()){
-          distance+= (it2->second-img2.getActivations()[it->first][it2->first])*(it2->second-img2.getActivations()[it->first][it2->first]);
-        }
-        //Else assume its value is 0
-        else distance+= (it2->second)*(it2->second);
-      }
-      else distance+= (it2->second)*(it2->second);
+  for(map<string,CNNLayer>::const_iterator l_it=layers.begin(); l_it!=layers.end(); l_it++){
+    //If layer does not exist in one image, error by now. TODO: Avoid error, assume al features are 0
+    if(img1.getActivations().find(l_it->first)==img1.getActivations().end()||
+        img2.getActivations().find(l_it->first)==img2.getActivations().end()){
+      printf("Util::euclideanDistanceActivations::ERROR::Image/s do not contain layer\n");
+      return 0;
     }
+    map<int,float> &act1 = img1.getActivations().find(l_it->first)->second;
+    map<int,float> &act2 = img2.getActivations().find(l_it->first)->second;
+    for(map<int,CNNFeature>::const_iterator f_it= l_it->second.getFeatures().begin(); f_it!= l_it->second.getFeatures().end(); f_it++){
+      float val1=0, val2=0;
+      if(act1.find(f_it->first)!=act1.end()) val1 = act1[f_it->first];
+      if(act2.find(f_it->first)!=act2.end()) val2 = act2[f_it->first];
+      distance+=(val1-val2)*(val1-val2);
+    } 
   }
   return sqrt(distance);
 }
