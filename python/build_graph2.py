2156f2f090f44a57e2ecfb21c62c98f265a12f9e
Dario Garcia <dariog@lsi.upc.edu>
2015-07-18 13:23:29 +0200

    -Added tools for cluster analysis in comparison with wordnet

--- python/build_graph2.py
+++ python/build_graph2.py
@@ -5,6 +5,8 @@ from scipy.cluster.hierarchy import linkage, dendrogram, leaves_list
 import numpy as np
 import csv
 from sklearn import manifold
+from nltk.corpus import wordnet as wn
+from nltk.corpus import genesis
 
 inf = open('labels.txt', 'r')
 labels = list()
@@ -35,196 +37,93 @@ for line in inf2:
 #    if sim[c1][c2] != sim[c2][c1]:
 #      print "diff " + str(labels[c1]) + " " + str(labels[c2])
 
+#Read labels and transform into NLTK compatible
+inf = open('labels.txt', 'r')
+labels = list()
+synsets = list()
+labelsNLTK = list()
+for line in inf:
+  labels.append(line)
+  labelsNLTK.append((line.split()[0])[1:]+'-n')
 
+#Obtain synsets
+for s1 in labelsNLTK:
+  synsets.append(wn.of2ss(s1))
+
+d_s = wn.synset('dog.n.01').closure(lambda s:s.hyponyms())
 dogs = []
-dogs.append("n02110341 dalmatian, coach dog, carriage dog\n")
-dogs.append("n02113978 Mexican hairless\n")
-dogs.append("n02111277 Newfoundland, Newfoundland dog\n")
-dogs.append("n02110806 basenji\n")
-dogs.append("n02111129 Leonberg\n")
-dogs.append("n02110958 pug, pug-dog\n")
-dogs.append("n02111500 Great Pyrenees\n")
-dogs.append("n02087394 Rhodesian ridgeback\n")
-dogs.append("n02100583 vizsla, Hungarian pointer\n")
-dogs.append("n02100236 German short-haired pointer\n")
-dogs.append("n02100735 English setter\n")
-dogs.append("n02100877 Irish setter, red setter\n")
-dogs.append("n02101006 Gordon setter\n")
-dogs.append("n02101556 clumber, clumber spaniel\n")
-dogs.append("n02101388 Brittany spaniel\n")
-dogs.append("n02102318 cocker spaniel, English cocker spaniel, cocker\n")
-dogs.append("n02102480 Sussex spaniel\n")
-dogs.append("n02102973 Irish water spaniel\n")
-dogs.append("n02102040 English springer, English springer spaniel\n")
-dogs.append("n02102177 Welsh springer spaniel\n")
-dogs.append("n02099601 golden retriever\n")
-dogs.append("n02099849 Chesapeake Bay retriever\n")
-dogs.append("n02099429 curly-coated retriever\n")
-dogs.append("n02099267 flat-coated retriever\n")
-dogs.append("n02099712 Labrador retriever\n")
-dogs.append("n02096294 Australian terrier\n")
-dogs.append("n02098105 soft-coated wheaten terrier\n")
-dogs.append("n02096437 Dandie Dinmont, Dandie Dinmont terrier\n")
-dogs.append("n02096051 Airedale, Airedale terrier\n")
-dogs.append("n02098413 Lhasa, Lhasa apso\n")
-dogs.append("n02094433 Yorkshire terrier\n")
-dogs.append("n02098286 West Highland white terrier\n")
-dogs.append("n02094114 Norfolk terrier\n")
-dogs.append("n02096177 cairn, cairn terrier\n")
-dogs.append("n02093859 Kerry blue terrier\n")
-dogs.append("n02097298 Scotch terrier, Scottish terrier, Scottie\n")
-dogs.append("n02096585 Boston bull, Boston terrier\n")
-dogs.append("n02093647 Bedlington terrier\n")
-dogs.append("n02093991 Irish terrier\n")
-dogs.append("n02097658 silky terrier, Sydney silky\n")
-dogs.append("n02094258 Norwich terrier\n")
-dogs.append("n02097474 Tibetan terrier, chrysanthemum dog\n")
-dogs.append("n02093754 Border terrier\n")
-dogs.append("n02095314 wire-haired fox terrier\n")
-dogs.append("n02095570 Lakeland terrier\n")
-dogs.append("n02095889 Sealyham terrier, Sealyham\n")
-dogs.append("n02097047 miniature schnauzer\n")
-dogs.append("n02097130 giant schnauzer\n")
-dogs.append("n02097209 standard schnauzer\n")
-dogs.append("n02093256 Staffordshire bullterrier, Staffordshire bull terrier\n")
-dogs.append("n02093428 American Staffordshire terrier, Staffordshire terrier, American pit bull terrier, pit bull terrier\n")
-dogs.append("n02092002 Scottish deerhound, deerhound\n")
-dogs.append("n02092339 Weimaraner\n")
-dogs.append("n02088466 bloodhound, sleuthhound\n")
-dogs.append("n02091635 otterhound, otter hound\n")
-dogs.append("n02091467 Norwegian elkhound, elkhound\n")
-dogs.append("n02091831 Saluki, gazelle hound\n")
-dogs.append("n02088094 Afghan hound, Afghan\n")
-dogs.append("n02088364 beagle\n")
-dogs.append("n02088238 basset, basset hound\n")
-dogs.append("n02088632 bluetick\n")
-dogs.append("n02090379 redbone\n")
-dogs.append("n02091244 Ibizan hound, Ibizan Podenco\n")
-dogs.append("n02090721 Irish wolfhound\n")
-dogs.append("n02090622 borzoi, Russian wolfhound\n")
-dogs.append("n02089078 black-and-tan coonhound\n")
-dogs.append("n02089867 Walker hound, Walker foxhound\n")
-dogs.append("n02089973 English foxhound\n")
-dogs.append("n02091032 Italian greyhound\n")
-dogs.append("n02091134 whippet\n")
-dogs.append("n02113023 Pembroke, Pembroke Welsh corgi\n")
-dogs.append("n02113186 Cardigan, Cardigan Welsh corgi\n")
-dogs.append("n02113624 toy poodle\n")
-dogs.append("n02113712 miniature poodle\n")
-dogs.append("n02113799 standard poodle\n")
-dogs.append("n02112706 Brabancon griffon\n")
-dogs.append("n02109047 Great Dane\n")
-dogs.append("n02108089 boxer\n")
-dogs.append("n02108422 bull mastiff\n")
-dogs.append("n02109961 Eskimo dog, husky\n")
-dogs.append("n02109525 Saint Bernard, St Bernard\n")
-dogs.append("n02105641 Old English sheepdog, bobtail\n")
-dogs.append("n02106382 Bouvier des Flandres, Bouviers des Flandres\n")
-dogs.append("n02106550 Rottweiler\n")
-dogs.append("n02105505 komondor\n")
-dogs.append("n02106030 collie\n")
-dogs.append("n02106166 Border collie\n")
-dogs.append("n02105855 Shetland sheepdog, Shetland sheep dog, Shetland\n")
-dogs.append("n02105412 kelpie\n")
-dogs.append("n02105251 briard\n")
-dogs.append("n02106662 German shepherd, German shepherd dog, German police dog, alsatian\n")
-dogs.append("n02105162 malinois\n")
-dogs.append("n02105056 groenendael\n")
-dogs.append("n02104365 schipperke\n")
-dogs.append("n02104029 kuvasz\n")
-dogs.append("n02107142 Doberman, Doberman pinscher\n")
-dogs.append("n02107312 miniature pinscher\n")
-dogs.append("n02110627 affenpinscher, monkey pinscher, monkey dog\n")
-dogs.append("n02110185 Siberian husky\n")
-dogs.append("n02110063 malamute, malemute, Alaskan malamute\n")
-dogs.append("n02108000 EntleBucher\n")
-dogs.append("n02107683 Bernese mountain dog\n")
-dogs.append("n02107574 Greater Swiss Mountain dog\n")
-dogs.append("n02107908 Appenzeller\n")
-dogs.append("n02108551 Tibetan mastiff\n")
-dogs.append("n02108915 French bulldog\n")
-dogs.append("n02112018 Pomeranian\n")
-dogs.append("n02112350 keeshond\n")
-dogs.append("n02112137 chow, chow chow\n")
-dogs.append("n02111889 Samoyed, Samoyede\n")
-dogs.append("n02086079 Pekinese, Pekingese, Peke\n")
-dogs.append("n02085936 Maltese dog, Maltese terrier, Maltese\n")
-dogs.append("n02087046 toy terrier\n")
-dogs.append("n02085782 Japanese spaniel\n")
-dogs.append("n02086240 Shih-Tzu\n")
-dogs.append("n02085620 Chihuahua\n")
-dogs.append("n02086910 papillon\n")
-dogs.append("n02086646 Blenheim spaniel\n")
+for d in d_s:
+  dogs.append(d)
 
-#strings = []
-#strings.append("n02787622 banjo\n")
-#strings.append("n02992211 cello, violoncello\n")
-#strings.append("n04536866 violin, fiddle\n")
-#strings.append("n03495258 harp\n")
-#strings.append("n02676566 acoustic guitar\n")
-#strings.append("n03272010 electric guitar\n")
-#
-#wind = []
-#wind.append("n03840681 ocarina, sweet potato\n")
-#wind.append("n03110669 cornet, horn, trumpet, trump\n")
-#wind.append("n03394916 French horn, horn\n")
-#wind.append("n04487394 trombone\n")
-#wind.append("n03494278 harmonica, mouth organ, harp, mouth harp\n")
-#wind.append("n03884397 panpipe, pandean pipe, syrinx\n")
-#wind.append("n03372029 flute, transverse flute\n")
-#wind.append("n02804610 bassoon\n")
-#wind.append("n03838899 oboe, hautboy, hautbois\n")
-#wind.append("n04141076 sax, saxophone\n")
-#wind.append("n04579432 whistle\n")
-
+w_s = wn.synset('wheeled_vehicle.n.01').closure(lambda s:s.hyponyms())
 wheeled = []
-wheeled.append("n03791053 motor scooter, scooter\n")
-wheeled.append("n04482393 tricycle, trike, velocipede\n")
-wheeled.append("n04509417 unicycle, monocycle\n")
-wheeled.append("n02835271 bicycle-built-for-two, tandem bicycle, tandem\n")
-wheeled.append("n03792782 mountain bike, all-terrain bike, off-roader\n")
-wheeled.append("n03393912 freight car\n")
-wheeled.append("n03895866 passenger car, coach, carriage\n")
-wheeled.append("n04204347 shopping cart\n")
-wheeled.append("n02797295 barrow, garden cart, lawn cart, wheelbarrow\n")
-wheeled.append("n03384352 forklift\n")
-wheeled.append("n04065272 recreational vehicle, RV, R.V.\n")
-wheeled.append("n04335435 streetcar, tram, tramcar, trolley, trolley car\n")
-wheeled.append("n04465501 tractor\n")
-wheeled.append("n04389033 tank, army tank, armored combat vehicle, armoured combat vehicle\n")
-wheeled.append("n03272562 electric locomotive\n")
-wheeled.append("n04310018 steam locomotive\n")
-wheeled.append("n02704792 amphibian, amphibious vehicle\n")
-wheeled.append("n03444034 go-kart\n")
-wheeled.append("n03445924 golfcart, golf cart\n")
-wheeled.append("n04252225 snowplow, snowplough\n")
-wheeled.append("n02701002 ambulance\n")
-wheeled.append("n02814533 beach wagon, station wagon, wagon, estate car, beach waggon, station waggon, waggon\n")
-wheeled.append("n02930766 cab, hack, taxi, taxicab\n")
-wheeled.append("n03100240 convertible\n")
-wheeled.append("n03594945 jeep, landrover\n")
-wheeled.append("n03670208 limousine, limo\n")
-wheeled.append("n03770679 minivan\n")
-wheeled.append("n03777568 Model T\n")
-wheeled.append("n04285008 sports car, sport car\n")
-wheeled.append("n04037443 racer, race car, racing car\n")
-wheeled.append("n03785016 moped\n")
-wheeled.append("n03417042 garbage truck, dustcart\n")
-wheeled.append("n03345487 fire engine, fire truck\n")
-wheeled.append("n03930630 pickup, pickup truck\n")
-wheeled.append("n04461696 tow truck, tow car, wrecker\n")
-wheeled.append("n04467665 trailer truck, tractor trailer, trucking rig, rig, articulated lorry, semi\n")
-wheeled.append("n03796401 moving van\n")
-wheeled.append("n03977966 police van, police wagon, paddy wagon, patrol wagon, wagon, black Maria\n")
-wheeled.append("n03478589 half track\n")
-wheeled.append("n04252077 snowmobile\n")
-wheeled.append("n03776460 mobile home, manufactured home\n")
-wheeled.append("n03538406 horse cart, horse-cart\n")
-wheeled.append("n03599486 jinrikisha, ricksha, rickshaw\n")
-wheeled.append("n03868242 oxcart\n")
-wheeled.append("n04487081 trolleybus, trolley coach, trackless trolley\n")
-wheeled.append("n03769881 minibus\n")
-wheeled.append("n04146614 school bus\n")
+for w in w_s:
+  wheeled.append(w)
+wheeled.append(wn.synset('school_bus.n.01'))
+wheeled.append(wn.synset('minibus.n.01'))
+wheeled.append(wn.synset('trolleybus.n.01'))
+
+a_s = wn.synset('animal.n.01').closure(lambda s:s.hyponyms())
+animals = []
+for a in a_s:
+  animals.append(a)
+#print "Num animals "+ str(len(animals))
+
+c_s = wn.synset('chordate.n.01').closure(lambda s:s.hyponyms())
+chordates = []
+for c in c_s:
+  chordates.append(c)
+#print "Num chordates "+ str(len(chordates))
+
+i_s = wn.synset('invertebrate.n.01').closure(lambda s:s.hyponyms())
+invertebrates = []
+for i in i_s:
+  invertebrates.append(i)
+#print "Num  invertebrates "+ str(len(invertebrates))
+
+ma_s = wn.synset('aquatic_vertebrate.n.01').closure(lambda s:s.hyponyms())
+marine_animals = []
+for ma in ma_s:
+  marine_animals.append(ma)
+#print "Num aquatic vertebrates "+ str(len(marine_animals))
+
+v_s = wn.synset('vertebrate.n.01').closure(lambda s:s.hyponyms())
+vertebrates = []
+for v in v_s:
+  vertebrates.append(v)
+#print "Num vertebrates "+ str(len(vertebrates))
+
+b_s = wn.synset('bird.n.01').closure(lambda s:s.hyponyms())
+birds = []
+for b in b_s:
+  birds.append(b)
+#print "Num birds "+ str(len(birds))
+
+r_s = wn.synset('reptile.n.01').closure(lambda s:s.hyponyms())
+reptiles = []
+for r in r_s:
+  reptiles.append(r)
+#print "Num reptiles "+ str(len(reptiles))
+
+vertebrates_nobird_nomarine_noreptile = list(vertebrates)
+for b in birds:
+  vertebrates_nobird_nomarine_noreptile.remove(b)
+for m in marine_animals:
+  vertebrates_nobird_nomarine_noreptile.remove(m)
+for r in reptiles:
+  vertebrates_nobird_nomarine_noreptile.remove(r)
+#print "Num vertebreates no-bird/marine/reptile "+ str(len(vertebrates_nobird_nomarine_noreptile))
+
+#animal 3998 
+#  chordate 3087
+#    vertebrate non-bird non-aquatic non-reptile 1278
+#    bird 871
+#    reptile 296
+#    aquatic vertebrate 632
+#    ? 10
+#  invertebrate 766
+#  animals_others 145
+
 
 l = []
 for i in range (1000):
@@ -236,16 +135,39 @@ adist /= amax
 
 colors = []
 for lab in labels:
-  if lab in dogs:
-    colors.append("k")
-  elif lab in wheeled:
-    colors.append("0.5")
-#  elif lab in strings:
-#    colors.append(2)
-#  elif lab in wind:
-#    colors.append(3)
+  labelNLTK = (lab.split()[0])[1:]+'-n'
+  syn = wn.of2ss(labelNLTK)
+#  if syn in chordates:
+#    colors.append("r")
+#  elif syn in invertebrates:
+#    colors.append("k")
+#  else:
+#    colors.append("w")
+
+#  if syn in vertebrates_nobird_nomarine_noreptile:
+#    colors.append("r")
+#  elif syn in reptiles:
+#    colors.append("g")
+#  elif syn in birds:
+#    colors.append("y")
+#  elif syn in marine_animals:
+#    colors.append("b")
+#  elif syn in invertebrates:
+#    colors.append("k")
+#  else:
+#    colors.append("w")
+
+  if syn in animals:
+    colors.append("y")
   else:
     colors.append("0.9")
+
+#  if syn in dogs:
+#    colors.append("k")
+#  elif syn in wheeled:
+#    colors.append("0.5")
+#  else:
+#    colors.append("0.9")
 
 
 mds = manifold.MDS(n_components=2, dissimilarity="precomputed", random_state=6)
@@ -256,17 +178,20 @@ coords = results.embedding_
 plt.figure(figsize=(60,60))
 plt.subplots_adjust(bottom = 0.1)
 plt.scatter(
-    coords[:, 0], coords[:, 1], marker = 'o', s=2000, c=colors
+    coords[:, 0], coords[:, 1], marker = 'o', s=3000, c=colors
     )
-plt.gray()
-##for label, x, y in zip(labels, coords[:, 0], coords[:, 1]):
-##  plt.annotate(
-##    label,
-##    xy = (x, y), xytext = (-20, 20),
-##    textcoords = 'offset points', ha = 'right', va = 'bottom',
-##    bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
-##    arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+#plt.gray()
+for label, x, y in zip(labels, coords[:, 0], coords[:, 1]):
+  labelNLTK = (label.split()[0])[1:]+'-n'
+  syn = wn.of2ss(labelNLTK)
+  if syn not in animals:
+    plt.annotate(
+      label,
+      xy = (x, y), xytext = (-10, 10),
+      textcoords = 'offset points', ha = 'right', va = 'bottom',
+      bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+      arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
 
 plt.show()
-plt.savefig('/media/dario/823D-2FFA/data_tiramisu/arith_cosine_classlayer_graph.pdf', format='pdf')
+plt.savefig('/media/dario/823D-2FFA/data_tiramisu/arith_cosine_classlayer_graph_tmp.pdf', format='pdf')
 
